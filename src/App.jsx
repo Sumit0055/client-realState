aff6a41a8cff9531440296698c6ca1bec0e35b7e
Sumit0055 <sumitduttsumitdutt@gmail.com>
2026-01-29 23:02:41 +0530

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
 import Error from './pages/Error'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/sign-in' element={<Signin />} />
@@ -24,3 +26,4 @@ export default function App() {
     </>
   )
 }
+// video paused = 28:46
